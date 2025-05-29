f0d905a1a824cce1561c064a6206c9d3827d5392
Ana Lívia <136380024+hellgby@users.noreply.github.com>
2025-05-29 15:04:13 -0300

    Add files via upload

---
+++ prova/prova1/provapratica1/provapratica1/bin/email_sender.dart
@@ -0,0 +1,20 @@
+email_sender.dart:
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server/gmail.dart';
+
+Future<void> enviarEmail(String conteudoJson) async {
+  final smtpServer = gmail('ana.candido09@aluno.ifce.edu.br', 'mggx nbxh firb nmjj');
+
+  final message = Message()
+    ..from = Address('ana.candido09@aluno.ifce.edu.br', 'ana livia')
+    ..recipients.add('taveira@ifce.edu.br')
+    ..subject = 'prova prática 1'
+    ..text = 'json do pedido:\n\n$conteudoJson';
+
+  try {
+    final sendReport = await send(message, smtpServer);
+    print('e-mail enviado com sucesso: $sendReport');
+  } on MailerException catch (e) {
+    print('erro ao enviar e-mail: ${e.toString()}');
+  }
+}
